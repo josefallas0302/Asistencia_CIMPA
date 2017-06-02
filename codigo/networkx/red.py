0c49aa86b98677d6672de4c60efafe41d556ba16
jose <ariel@talista.arcoslab.eie.loc>
2017-06-02 17:55:33 -0600

    Implementando la red con networkx

---
+++ codigo/networkx/red.py
@@ -0,0 +1,26 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import random
+
+def generate(num_nodes,num_edges):
+	G = nx.Graph() 
+	for i in range(num_nodes):
+		G.add_node(i)
+
+	for i in range(num_edges):
+		G.add_edge(random.randint(0,num_nodes),random.randint(0,num_nodes))
+
+	return G 
+if __name__ == "__main__":
+	
+	n_nodes = int(input("Inserte el numero de nodos que desea:"))
+
+	
+	n_edges = 	int(input("Inserte el numero de enlaces que desea:"))
+
+
+	red_1= generate(n_nodes,n_edges)
+
+	nx.draw(red_1)
+	red_1.draw_networkx_labels()
+	plt.show()
---
+++ codigo/plan_A/graficos.py
@@ -0,0 +1,14 @@
+import gg
+
+circleFun <- function(center = c(0,0),diameter = 1, npoints = 100){
+    r = diameter / 2
+    tt <- seq(0,2*pi,length.out = npoints)
+    xx <- center[1] + r * cos(tt)
+    yy <- center[2] + r * sin(tt)
+    return(data.frame(x = xx, y = yy))
+}
+
+
+dat <- circleFun(c(1,-1),2.3,npoints = 100)
+#geom_path will do open circles, geom_polygon will do filled circles
+ggplot(dat,aes(x,y)) + geom_path()
